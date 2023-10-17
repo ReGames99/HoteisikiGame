28ad05c1a991b501dff1d53dca9fc80eeaf7be2d
Regames99 <kengonosyoutai@gmail.com>
2023-10-17 19:01:52 +0900

    セパレート時に親オブジェクトを設定

--- Assets/Scripts/SeparateNum.cs
+++ Assets/Scripts/SeparateNum.cs
@@ -16,9 +16,13 @@ public class SeparateNum : MonoBehaviour
 
     public void SeparateNumberToBalls()
     {
-        Instantiate((GameObject)Resources.Load("x"), new Vector3(gameObject.transform.position.x + 0.8f, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity, gameObject.transform.parent);
+        GameObject variableX = Instantiate((GameObject)Resources.Load("x"), new Vector3(gameObject.transform.position.x + 0.8f, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity, gameObject.transform.parent);
         numcir = Instantiate((GameObject)Resources.Load("numcir"), new Vector3(gameObject.transform.position.x - 0.8f, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity, gameObject.transform.parent);
         numcir.GetComponent<MultiDiv>().symbolX = Instantiate((GameObject)Resources.Load("X 1"), new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity, gameObject.transform.parent);
+
+        variableX.transform.localScale = new Vector3(0.5f, 0.5f, 0);
+        numcir.transform.localScale = new Vector3(0.5f, 0.5f, 0);
+        numcir.GetComponent<MultiDiv>().symbolX.transform.localScale = new Vector3(0.01f, 0.01f, 0);
 
         numcir.name = gameObject.GetComponent<MyNum>().myNum.ToString();
         numcir.gameObject.GetComponent<MyNum>().SetMyNumber();

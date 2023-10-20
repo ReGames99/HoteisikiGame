6dcd5da4509422850155018620e47208a5367b31
Regames99 <kengonosyoutai@gmail.com>
2023-10-20 16:20:56 +0900

    戻せるようにした

--- Assets/Scripts/SeparateNum.cs
+++ Assets/Scripts/SeparateNum.cs
@@ -18,7 +18,17 @@ public class SeparateNum : MonoBehaviour
     {
         GameObject variableX = Instantiate((GameObject)Resources.Load("x"), new Vector3(gameObject.transform.position.x + 0.8f, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity, gameObject.transform.parent);
         numcir = Instantiate((GameObject)Resources.Load("numcir"), new Vector3(gameObject.transform.position.x - 0.8f, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity, gameObject.transform.parent);
-        GameObject symbolX = Instantiate((GameObject)Resources.Load("X 1"), new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity, gameObject.transform.parent);
+        
+        if(gameObject.transform.parent.Find("X 1(Clone)"))
+        {
+            gameObject.transform.parent.Find("X 1(Clone)").gameObject.SetActive(true);
+        }
+        else
+        {
+            GameObject symbolX = Instantiate((GameObject)Resources.Load("X 1"), new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity, gameObject.transform.parent);
+            symbolX.transform.localScale = new Vector3(0.01f, 0.01f, 0);
+        }
+        
 
         //numcirÇï™éqÇ…ê›íË
         numcir.GetComponent<MyNum>().motherOrChildFlag = true;
@@ -26,7 +36,7 @@ public class SeparateNum : MonoBehaviour
         //ëÂÇ´Ç≥ÇÃí≤êÆ
         variableX.transform.localScale = new Vector3(0.5f, 0.5f, 0);
         numcir.transform.localScale = new Vector3(0.5f, 0.5f, 0);
-        symbolX.transform.localScale = new Vector3(0.01f, 0.01f, 0);
+        
 
         numcir.name = gameObject.GetComponent<MyNum>().myNum.ToString();
         numcir.gameObject.GetComponent<MyNum>().SetMyNumber();

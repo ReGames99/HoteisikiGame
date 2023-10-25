ec1314178841cf3aef834e23b18a8d8b0a3b32fc
Regames99 <kengonosyoutai@gmail.com>
2023-10-25 18:53:10 +0900

    no message

---
+++ Assets/Scripts/GrabParent.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabParent : MonoBehaviour
+{
+    bool aloneFlag;
+
+    Vector3 initialPos;
+
+    private void Start()
+    {
+        initialPos = transform.parent.position;
+    }
+
+    private void OnMouseDown()
+    {
+        if(transform.parent.childCount == 1)
+        {
+            aloneFlag = true;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        if (aloneFlag == true)
+        {
+            transform.parent.position = transform.position;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (aloneFlag == true)
+        {
+            transform.parent.position = initialPos;
+        }
+    }
+
+
+}
